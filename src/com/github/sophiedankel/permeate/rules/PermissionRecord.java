d5ce681952a4b4d676e601e20a29c5ff800c7f3a
Sophie Dankel <dankels@seattleu.edu>
2014-01-14 11:45:17 -0800

    Add APICall data structures

---
+++ src/com/github/sophiedankel/permeate/rules/PermissionRecord.java
@@ -0,0 +1,41 @@
+package com.github.sophiedankel.permeate.rules;
+
+
+public class PermissionRecord 
+{
+	private static final String PERMISSION_PATH = "android.permission";
+	private String name;
+	private Boolean isRequired;
+	public PermissionRecord(String permName, Boolean required) 
+	{
+		name = PERMISSION_PATH + permName;
+		isRequired = required;
+	}
+	public PermissionRecord() 
+	{
+		name = PERMISSION_PATH;
+		isRequired = true;
+	}
+	public String toString()
+	{
+		String result = name + "\t";
+		if (isRequired)
+			result += "REQUIRED";
+		else
+			result += "OPTIONAL";
+		return result;
+	}
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String permName) {
+		name += permName;
+	}
+	public Boolean isRequired() {
+		return isRequired;
+	}
+	public void setIsRequired(boolean required) {
+		isRequired = required;
+	}
+}
